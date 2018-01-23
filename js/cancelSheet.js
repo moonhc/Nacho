4792d602c809e5aabd8eb0e6361ae05bde8be812
moonhc <mhc3357@gmail.com>
2018-01-23 11:41:56 +0900

    Construct the structure for making cancle sheets

---
+++ js/cancelSheet.js
@@ -0,0 +1,31 @@
+excel make
+excel write
+excel download
+
+function createExcel() {
+	let workbook;
+	return workbook;
+}
+
+function createCancelSheets(workbook, cancelData) {
+	let refinedData = refineCancelData(cancelData);
+	writeToSheet(workbook, )
+	return workbook;
+}
+
+function writeToSheet(workbook, data) {
+	return workbook;
+}
+
+function downloadExcel() {
+
+}
+
+function findSheetByName(workbook, sheetName) {
+	return;
+}
+
+function refineCancelData(cancelData) {
+	let refinedData;
+	return refinedData;
+}
