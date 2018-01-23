23b826608ca9c7e1c2f7ce60b2675ba06b8dae11
moonhc <mhc3357@gmail.com>
2018-01-24 08:57:36 +0900

    Add error handling for fileName

--- js/cancelSheet.js
+++ js/cancelSheet.js
@@ -7,22 +7,39 @@ function createExcel(cancelData) {
 
 function createCancelSheets(workbook, cancelData) {
 	let refinedData = refineCancelData(cancelData);
-	writeToSheet(workbook, )
+
+	for (let type in refinedData) {
+		let sheetName = `${type}(취소)`;
+		const sheet = workbook.addSheet(sheetName);
+		writeToSheet(sheet, refinedData[type]);
+	}
+}
+
+function writeToSheet(sheet, data) {
 	return workbook;
 }
 
-function writeToSheet(workbook, data) {
-	return workbook;
-}
+function downloadExcel(workbook, fileName) {
+	// Error handling for file name
+	if (fileName.indexOf('.') == -1) {
+		alert(`File name has no extension`);
+		return;
+	}
+	let splitFileName = fileName.split('.');
+	let extension = splitFileName[splitFileName.length-1];
+	if (extension != 'xlsx' && extension != 'xls') {
+		alert(`File extension (${extension}) is not supported`);
+		return;
+	}
 
-function downloadExcel(workbook) {
+	// Download file with given file name
 	workbook.outputAsync().then(
 		function (blob) {
 			let url = window.URL.createObjectURL(blob);
 			let a = document.createElement('a');
 			document.body.appendChild(a);
 			a.href = url;
-			a.download = 'out.xlsx';
+			a.download = fileName;
 			a.click();
 			window.URL.revokeObjecURL(url);
 			document.body.removeChild(a);
